fe1bb623134c8522f69c6c9be30eafcb0d6f5b70
SombraXxxX <43217593+SombraXxxX@users.noreply.github.com>
2019-03-01 14:47:21 -0500

    Update SearchAndSort.java

--- SearchAndSort.java
+++ SearchAndSort.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SearchAndSort {
@@ -16,18 +18,38 @@ public class SearchAndSort {
 		String algorthim = in.nextLine();
 		
 		System.out.println("What type of data? Acceptable responses: integers, strings.");
-		String dataType = in.nextLine();
-		System.out.println("How is it stored? Acceptable responses array, list.");
-		String stored = in.nextLine();
+		String dataType = in.nextLine().toLowerCase();
+		System.out.println("How is it stored? Acceptable responses array, arraylist.");
+		String stored = in.nextLine().toLowerCase();
 		System.out.println(" Enter the data. Acceptable responses: a comma-delimited list of data (i.e., 1,2,3,4,5).");
 		String data = in.nextLine();
-		if(dataType == "integers"){
-			
+		if(dataType.equals("strings") && stored.equals("arraylist" )){
+			String[] intstrings = data.split(",");
+				ArrayList<String> strings = new ArrayList<String>();
+				for(int i=0; i<intstrings.length; i++) {
+					strings.add(intstrings[i]);
+				}
+				System.out.print(Arrays.toString(strings.toArray()));
+		}else if(dataType.equals("integers") && stored.equals("array")) {
+			String[] stringints = data.split(",");
+			 int ints[] = new int[stringints.length]; 
+		        for (int i=0; i<stringints.length; i++) 
+		            ints[i] = Integer.parseInt(stringints[i]);
+			System.out.println(Arrays.toString(ints));
+
+		}else if (dataType.equals("strings") && stored.equals("array")) {
+			String[] strings = data.split(",");
+			System.out.println(Arrays.toString(strings));
+		}else if(dataType.equals("integers") && stored.equals("arraylist" )) {
+			String[] stringints = data.split(",");
+			ArrayList<Integer> ints = new ArrayList<Integer>();
+			for(int i=0; i<stringints.length; i++) {
+				ints.add(stringints[i]);
+			}
+			System.out.print(Arrays.toString(ints.toArray()));
 		}else {
-			
+			System.out.print("error");
 		}
 
-
-		
 	}
 }
